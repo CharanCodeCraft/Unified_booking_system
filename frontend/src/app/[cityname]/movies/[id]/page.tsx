f135475450bd845c52bf568df42ac213af7e7d11
CharanCodeCraft <charangowak911@gmail.com>
2025-04-24 12:48:19 +0530

    Movie single card

--- frontend/src/app/[cityname]/movies/[id]/page.tsx
+++ frontend/src/app/[cityname]/movies/[id]/page.tsx
@@ -1,49 +1,167 @@
-"use client"
-import { useParams, usePathname } from 'next/navigation'
-import React from 'react'
-import { Star } from 'lucide-react';
+"use client";
+import { useParams, usePathname } from "next/navigation";
+import React from "react";
+import { Star } from "lucide-react";
 function moviecard() {
-    // const pathname = usePathname()
-    // const { movieid } = useParams()
-    const moviedetails = {
-            "_id": "65101a2acc5b257e6f2816a5",
-            "title": "Kesari Chapter 2: The Untold Story of Jallianwala Bagh",
-            "description": "A high-octane action thriller that outlines the emotional journey of a man who is set to rectify the wrongs in society.",
-            "portraitImgUrl": "https://assets-in.bmscdn.com/iedb/movies/images/mobile/thumbnail/xlarge/kesari-chapter-2-the-untold-story-of-jallianwala-bagh-et00439158-1744618984.jpg",
-            "landscapeImgUrl": "https://assets-in.bmscdn.com/iedb/movies/images/mobile/listing/xxlarge/kesari-chapter-2-the-untold-story-of-jallianwala-bagh-et00439158-1744618984.jpg",
-            "rating": 8,
-            "genre": [
-                "Action",
-                "Thriller"
-            ],
-            "duration": 130,
-            "cast": [],
-            "crew": [],
-            "__v": 0
-        }
+  // const pathname = usePathname()
+  // const { movieid } = useParams()
+  const moviedetails = {
+    _id: "65101a2acc5b257e6f2816a5",
+    title: "Kesari Chapter 2: The Untold Story of Jallianwala Bagh",
+    description:
+      "A high-octane action thriller that outlines the emotional journey of a man who is set to rectify the wrongs in society.",
+    portraitImgUrl:
+      "https://assets-in.bmscdn.com/iedb/movies/images/mobile/thumbnail/xlarge/kesari-chapter-2-the-untold-story-of-jallianwala-bagh-et00439158-1744618984.jpg",
+    landscapeImgUrl:
+      "https://assets-in.bmscdn.com/iedb/movies/images/mobile/listing/xxlarge/kesari-chapter-2-the-untold-story-of-jallianwala-bagh-et00439158-1744618984.jpg",
+    rating: 8,
+    genre: ["Action", "Thriller"],
+    duration: 130,
+    cast: [],
+    crew: [],
+    __v: 0,
+  };
   return (
     <div>
-     <div
-  className="relative bg-cover bg-center h-[20%] w-full"
-  style={{ backgroundImage: `url(${moviedetails.landscapeImgUrl})` }}
->
-  <div className="absolute inset-0 bg-black opacity-60"/>
-  <div className="relative z-10 p-6 text-white flex gap-10">
-    <div className="img"><img src={moviedetails.portraitImgUrl} alt="image" className='border-0 rounded-2xl'/></div>
-    <div className="moviedetails flex flex-col gap-5 w-[50%] pt-7">
-    <h1 className="text-4xl font-bold ">{moviedetails.title}</h1>
-    <div className="rating bg-gray-700 w-[40%] flex justify-between items-center pr-5 pl-5 p-2 gap-1 border-0 rounded-[5px]" >
-      <div className="rating flex gap-1">
-      <Star />  
-      <p>{moviedetails.rating}/10</p>
+      <div
+        className="relative bg-cover bg-center h-[20%] w-full"
+        style={{ backgroundImage: `url(${moviedetails.landscapeImgUrl})` }}
+      >
+        <div className="absolute inset-0 bg-black opacity-60" />
+        <div className="relative z-10 p-6 text-white flex gap-10">
+          <div className="img">
+            <img
+              src={moviedetails.portraitImgUrl}
+              alt="image"
+              className="border-0 rounded-2xl"
+            />
+          </div>
+          <div className="moviedetails flex flex-col gap-5 w-[50%] pt-7">
+            <h1 className="text-4xl font-bold ">{moviedetails.title}</h1>
+            <div className="rating bg-gray-700 w-[40%] flex justify-between items-center pr-5 pl-5 p-2 gap-1 border-0 rounded-[5px]">
+              <div className="rating flex gap-1">
+                <Star />
+                <p>{moviedetails.rating}/10</p>
+              </div>
+              <button className="bg-white text-black border-0 rounded-[3px] p-[3px]">
+                Rate now
+              </button>
+            </div>
+            <div className="duration flex gap-2">
+              <p>{moviedetails.duration} min</p>
+              <p>
+                {moviedetails.genre[0]}/{moviedetails.genre[1]}
+              </p>
+            </div>
+            <div className="bookticket bg-rose-500 w-[20%] p-2 border-0 cursor-pointer rounded-[5px] font-bold text-center">
+              <button>Book ticket</button>
+            </div>
+          </div>
+        </div>
       </div>
-      <button className='bg-white text-black border-0 rounded-[3px] p-[3px]'>Rate now</button>
+      <div className="cast">
+        <div className="desc m-5 ml-15 w-[60%]">
+          <h1 className="font-bold text-2xl">About the movie</h1>
+          <p className="mt-3">
+            Inspired by the annals of history, this is the riveting tale of
+            Sankaran Nair, a fearless and charismatic barrister who waged an
+            epic legal battle against the mighty British Empire, exposing to the
+            world the brutal truth behind the Jallianwala Bagh Massacre.
+          </p>
+        </div>
+        <hr className="w-[70%] ml-15 opacity-40" />
+        <div className="castnames ml-15 m-5">
+          <h1 className="font-bold text-2xl mb-5">Cast</h1>
+          <div className="imgdiv flex gap-10">
+            <div className="imges w-36">
+              <div className="roles flex flex-col justify-center items-center">
+                <img
+                  src="https://assets-in.bmscdn.com/iedb/artist/images/website/poster/large/akshay-kumar-94-1681713982.jpg"
+                  className="border-0 rounded-full"
+                  alt=""
+                />
+                <p className="font-bold">Akshay Kumar</p>
+                <p className="text-center text-gray-500">
+                  as Chettur Sankaran Nair
+                </p>
+              </div>
+            </div>
+            <div className="imges w-36 ">
+              <div className="roles flex flex-col justify-center items-center">
+                <img
+                  src="https://assets-in.bmscdn.com/iedb/artist/images/website/poster/large/akshay-kumar-94-1681713982.jpg"
+                  className="border-0 rounded-full"
+                  alt=""
+                />
+                <p className="font-bold">Akshay Kumar</p>
+                <p className="text-center text-gray-500">
+                  as Chettur Sankaran Nair
+                </p>
+              </div>
+            </div>
+            <div className="imges w-36 ">
+              <div className="roles flex flex-col justify-center items-center">
+                <img
+                  src="https://assets-in.bmscdn.com/iedb/artist/images/website/poster/large/akshay-kumar-94-1681713982.jpg"
+                  className="border-0 rounded-full"
+                  alt=""
+                />
+                <p className="font-bold">Akshay Kumar</p>
+                <p className="text-center text-gray-500">
+                  as Chettur Sankaran Nair
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <hr className="w-[70%] ml-15 opacity-40" />
+        <div className="castnames ml-15 m-5">
+          <h1 className="font-bold text-2xl mb-5">Crew</h1>
+          <div className="imgdiv flex gap-10">
+            <div className="imges w-36">
+              <div className="roles flex flex-col justify-center items-center">
+                <img
+                  src="https://assets-in.bmscdn.com/iedb/artist/images/website/poster/large/akshay-kumar-94-1681713982.jpg"
+                  className="border-0 rounded-full"
+                  alt=""
+                />
+                <p className="font-bold">Akshay Kumar</p>
+                <p className="text-center text-gray-500">
+                  as Chettur Sankaran Nair
+                </p>
+              </div>
+            </div>
+            <div className="imges w-36 ">
+              <div className="roles flex flex-col justify-center items-center">
+                <img
+                  src="https://assets-in.bmscdn.com/iedb/artist/images/website/poster/large/akshay-kumar-94-1681713982.jpg"
+                  className="border-0 rounded-full"
+                  alt=""
+                />
+                <p className="font-bold">Akshay Kumar</p>
+                <p className="text-center text-gray-500">
+                  as Chettur Sankaran Nair
+                </p>
+              </div>
+            </div>
+            <div className="imges w-36 ">
+              <div className="roles flex flex-col justify-center items-center">
+                <img
+                  src="https://assets-in.bmscdn.com/iedb/artist/images/website/poster/large/akshay-kumar-94-1681713982.jpg"
+                  className="border-0 rounded-full"
+                  alt=""
+                />
+                <p className="font-bold">Akshay Kumar</p>
+                <p className="text-center text-gray-500">
+                  as Chettur Sankaran Nair
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-    </div>
-  </div>
-</div>
-    </div>  
-  )
+  );
 }
 
-export default moviecard
+export default moviecard;
